df60004f4b629a0188777a7546b20c592db6da01
Zoran Simonovic <simonovic1980@gmail.com>
2023-03-14 22:04:30 +0100

    testig server api

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
 
 const app = express();
 dotenv.config();
@@ -16,6 +17,8 @@ const connect = async () => {
 };
 
 app.use(express.json());
+
+app.use("/api/users", userRoute);
 
 app.listen(8800, () => {
   connect();
