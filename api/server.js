25220512f9d800dd722b08bebb79d4d84b3314f5
Zoran Simonovic <simonovic1980@gmail.com>
2023-03-14 17:53:44 +0100

    models

--- api/server.js
+++ api/server.js
@@ -1,7 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+mongoose.set("strictQuery", true);
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("MongoDB is connected!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.use(express.json());
 
 app.listen(8800, () => {
+  connect();
   console.log("Backend server is running!");
 });
