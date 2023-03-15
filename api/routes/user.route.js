45465614deac63558ebdad27c078364c9b520589
Zoran Simonovic <simonovic1980@gmail.com>
2023-03-15 08:18:16 +0100

    controlers & routes

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { deleteUser } from "../controllers/user.controller.js";
+import { fn } from "../controllers/user.controller.js";
 // import { register, login, logout } from "../controllers/auth.controller.js";
 
 const router = express.Router();
-router.get("/test", deleteUser);
+router.get("/test", fn);
 
 // router.post("/register", register);
 // router.post("/login", login);
