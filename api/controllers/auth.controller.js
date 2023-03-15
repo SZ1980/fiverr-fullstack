0dbad0130a60f842974dabaf97f3948caf582d95
Zoran Simonovic <simonovic1980@gmail.com>
2023-03-15 17:48:08 +0100

    JWT & Cookies

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,3 +1,49 @@
-export const fn = (req, res) => {
-  res.send("from controllers!!!");
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  try {
+    const hash = bcrypt.hashSync(req.body.password, 5);
+    const newUser = new User({
+      ...req.body,
+      password: hash,
+    });
+
+    await newUser.save();
+    res.status(200).send("User has been created!");
+  } catch (err) {
+    res.status(500).send("Something went wrong!");
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return res.status(404).send("User Not Found!");
+
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+    if (!isCorrect)
+      return res.status(401).send("Username ore Password are incorrect!");
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        isSeller: user.isSeller,
+      },
+      process.env.JWT_KEY
+    );
+
+    const { password, ...info } = user._doc;
+    res.cookie("accessToken", token, { httpOnly: true }).status(200).send(info);
+  } catch (err) {
+    res.status(500).send("Something went wrong!");
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).send("Something went wrong!");
+  }
 };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,10 @@
 import express from "express";
-import { fn } from "../controllers/auth.controller.js";
+import { register, login, logout } from "../controllers/auth.controller.js";
 
 const router = express.Router();
-router.get("/test", fn);
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
